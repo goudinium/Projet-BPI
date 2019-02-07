e98c2cd0e69c8f46fa91c58add0a025a26952b6e
François Goudineau <Francois.Goudineau@grenoble-inp.org>
2019-02-07 16:05:06 +0100

    Ajout de la fonction permettant de générer des lignes aléatoirement

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
-import svg
-import point
-
 '''
 Fichier principal
 '''
 
+from geometry import random_line
+from svg import SVG
+
+
+def generate_random_lines(nb_lines=7):
+    '''
+    Fonction permettant de générer un nombre n de lignes aléatoires
+    Entrée:
+        nb_lines: nombre n de lignes à générer aléatoirement
+    Sortie:
+        lines: un tableau contenant les objets lignes précédemment générés
+    '''
+    return [random_line() for _ in nb_lines]
+
 
 def main():
-    pass
+    '''
+    Fonction principale
+    '''
+    drawing = SVG("dessin.svg", 1000, 1000)
+    drawing.header()
+
+    lines = generate_random_lines()
+    for line in lines:
+        drawing.draw_line(line)
+
+    drawing.footer()
+    drawing.write_file()
 
 if __name__ == '__main__':
     main()
