8a310573e43c5114d085c7f6e095ac43254bb670
François Goudineau <Francois.Goudineau@grenoble-inp.org>
2019-02-07 16:22:38 +0100

    ajout de nouveaux tests

--- tests.py
+++ tests.py
@@ -4,7 +4,7 @@ développement
 '''
 
 from svg import SVG
-from point import Point
+from line import random_line
 
 dessin = SVG("dessin.svg", 300, 300)
 
@@ -13,9 +13,14 @@ def test_line():
     '''
     Fonction permettant de tester l
     '''
-    point_1 = Point(100, 100)
-    point_2 = Point(200, 200)
-    dessin.draw_line(point_1, point_2)
+    ligne = random_line()
+    dessin.draw_line(ligne)
+
+
+def test_multiple_random():
+    for _ in range(7):
+        ligne = random_line()
+        dessin.draw_line(ligne)
 
 
 def main():
@@ -23,7 +28,7 @@ def main():
     Fonction principale
     '''
     dessin.header()
-    test_line()
+    test_multiple_random()
     dessin.footer()
     dessin.write_file()
 
