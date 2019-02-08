258d0a0a473a89f61ef609bce3396ae4ecb4d8b7
François Goudineau <Francois.Goudineau@grenoble-inp.org>
2019-02-08 12:07:47 +0100

    fin du développement du code lié à la symétrie des éléments

--- geometry.py
+++ geometry.py
@@ -12,12 +12,32 @@ class Point:
     Classe point permettant de représenter des points sur le dessin
     '''
 
-    def __init__(self, abscisse, ordonnee):
+    def __init__(self, abscisse=0, ordonnee=0):
         '''
         Constructeur de la classe
         '''
         self.x = abscisse
         self.y = ordonnee
+
+    def __str__(self):
+        '''
+        Méthode srt pour afficher les coordonnées du point
+        '''
+        result = "{},{}".format(self.x, self.y)
+        return result
+
+    def get_symmetrical(self, point):
+        '''
+        Méthode permettant d'obtenir le symétrique du point en question à
+        partir d'un second point passé en argument
+        '''
+        x_difference = self.x - point.x
+        y_difference = self.y - point.y
+
+        new_x = self.x - 2 * x_difference
+        new_y = self.y - 2 * y_difference
+        symmetrical = Point(new_x, new_y)
+        return symmetrical
 
 
 class Line:
@@ -33,36 +53,40 @@ class Line:
         self.point_1 = point_1
         self.point_2 = point_2
 
+    def __str__(self):
+        '''
+        Méthode str pour afficher les points constituant les extrémités du
+        segment. Sert essentiellement pour debuguer le code
+        '''
+        result = "Point 1 : {}\nPoint 2 : {}".format(
+            self.point_1, self.point_2)
+        return result
+
     def get_symmetrical(self, point):
         '''
-        Méthode permettant de récupérer le symétrique du trait par rapport à la
-        ligne formée entre un point passé en argument et l'axe des abscisse
+        Méthode permettant de récupérer le symétrique du trait par rapport à un
+        point passé en argument
         '''
-        intersect_1 = Point(point.x, self.point_1.y)
-        intersect_2 = Point(point.x, self.point_2.y)
+        point_1 = self.point_1.get_symmetrical(point)
+        point_2 = self.point_2.get_symmetrical(point)
 
-        distance_1 = sqrt((self.point_1.x - intersect_1.x)**2 +
-                          (self.point_1.x - intersect_1.x)**2)
-        distance_2 = sqrt((self.point_2.x - intersect_2.x)**2 +
-                          (self.point_2.x - intersect_2.x)**2)
-
-        new_point_1 = Point(intersect_1.x - distance_1, self.point_1.y)
-        new_point_2 = Point(intersect_2.x - distance_2, self.point_2.y)
-
-        return Line(new_point_1, new_point_2)
+        return Line(point_1, point_2)
 
 
-def random_line(max_abscisse=300, max_ordonnee=300):
+def random_point(x_interval=(0, 300), y_interval=(0, 300)):
+    '''
+    Fonction permettant de générer un point aléatoire
+    '''
+
+    x = randint(x_interval[0], x_interval[1])
+    y = randint(y_interval[0], y_interval[1])
+    return Point(x, y)
+
+
+def random_line(x_interval=(0, 300), y_interval=(0, 300)):
     '''
     Fonction permettant de générer une ligne aléatoire
     '''
-    x_1 = randint(0, max_abscisse)
-    x_2 = randint(0, max_abscisse)
 
-    y_1 = randint(0, max_ordonnee)
-    y_2 = randint(0, max_ordonnee)
-
-    point_1 = Point(x_1, y_1)
-    point_2 = Point(x_2, y_2)
-
-    return Line(point_1, point_2)
+    return Line(random_point(x_interval, y_interval),
+                random_point(x_interval, y_interval))
